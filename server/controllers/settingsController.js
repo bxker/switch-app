23283071a8cc274e06cdc49380777ea0ce55d2cb
bxker <jake.luke.baker@gmail.com>
2019-09-21 18:32:08 -0500

    added error catching for multiple usernames

--- server/controllers/settingsController.js
+++ server/controllers/settingsController.js
@@ -2,9 +2,15 @@ const updateUsername = async (req, res) => {
     const db = req.app.get('db');
     const {username} = req.body;
     const {user_id} = req.session.user;
-    await db.settings.updateUsername(username, user_id);
-    res.sendStatus(200);
+    try {
+        await db.settings.updateUsername(username, user_id);
+        res.sendStatus(200);
+    }
+    catch{
+        res.status(409).json('Username taken!')
+    }
 }
+
 const updateStreamTitle = async (req, res) => {
     const db = req.app.get('db');
     const {stream_title} = req.body;
@@ -12,6 +18,7 @@ const updateStreamTitle = async (req, res) => {
     await db.settings.updateStreamTitle(stream_title, user_id);
     res.sendStatus(200)
 }
+
 const updateTwitchUsername = (req, res) => {
     const db = req.app.get('db');
 }
