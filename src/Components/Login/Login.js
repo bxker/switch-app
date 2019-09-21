7281c2a0386e9fe1d15123ee6711e7f2c4665ca0
bxker <jake.luke.baker@gmail.com>
2019-09-20 21:23:24 -0500

    finished styling landing, login, and register

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import "./Login.sass"
 
 export default class Login extends Component {
     render() {
         return (
-            <div>
-                <h1>Login</h1>
+            <div className="register-main">
+                <section className="register-title">
+                    <h1>Log In to <span className="switch-green"> Switch</span> </h1>
+                </section>
+                <section className="green-box">
+                    <section className="section-1-register">
+                        <div className="register-left">
+                            <h2>Username</h2>
+                            <input></input>
+                            <h2>Password</h2>
+                            <input type="password"></input>
+                        </div>
+                    </section>
+                    <section className="section-2-register section-2-login">
+                        <button>Login</button>
+                        <h1>Don't have an account? Sign up here: <a href="/#/register">Register</a></h1>
+                    </section>
+                </section>
             </div>
         )
     }
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
+import './Register.sass';
 
 export default class Register extends Component {
     render() {
         return (
-            <div>
-                <h1>Register</h1>
+            <div className="register-main">
+                <section className="register-title">
+                    <h1>Sign up for <span className="switch-green"> Switch</span> </h1>
+                </section>
+                <section className="green-box">
+                    <section className="section-1-register">
+                        <div className="register-left">
+                            <h2>First Name</h2>
+                            <input></input>
+                            <h2>Last Name</h2>
+                            <input></input>
+                            <h2>Username</h2>
+                            <input></input>
+                        </div>
+                        <div className="register-right">
+                            <h2>Email</h2>
+                            <input></input>
+                            <h2>Password</h2>
+                            <input type="password"></input>
+                            <h2>Favorite Color</h2>
+                            <input></input>
+                        </div>
+                    </section>
+                    <section className="section-2-register">
+                        <button>Sign Up</button>
+                        <h1>Already have an account? <a href="/#/login">Login</a></h1>
+                    </section>
+                </section>
             </div>
         )
     }
