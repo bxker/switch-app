ec59accade9452837d2aa025f67abec9e999117e
bxker <jake.luke.baker@gmail.com>
2019-09-28 12:29:44 -0500

    added static path for build

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env;
 
 //middleware
 app.use(express.json());
+app.use( express.static( `${__dirname}/../build` ) );
 
 app.use(session({
     secret: SESSION_SECRET,
